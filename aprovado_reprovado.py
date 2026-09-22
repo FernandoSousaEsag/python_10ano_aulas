84587635c00e1f7419db8623c068c6f452ef1a3d
Fernando_Sousa_ESAG <95343917+FernandoSousaEsag@users.noreply.github.com>
2026-09-22 09:53:36 +0100

    Add files via upload

---
+++ aprovado_reprovado.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")  # limpar o ecrã do terminal (funciona no Windows)
+
+nota1 = float(input("Introduza a primeira nota: "))
+nota2 = float(input("Introduza a segunda nota: "))
+nota3 = float(input("Introduza a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+print(f"Média: {media:.1f}")
+
+if media >= 10:
+    print("Aluno aprovado")
+else:
+    print("Aluno reprovado")
---
+++ maior_2_numeros.py
@@ -0,0 +1,8 @@
+num1 = int(input("Introduza o primeiro número: "))
+num2 = int(input("Introduza o segundo número: "))
+
+if num1 > num2:
+    print("O maior número é:", num1)
+else:
+    print("O maior número é:", num2)
+    
