cca03f653f7480c92860086c88b263ce455f13b8
Fernando_Sousa_ESAG <95343917+FernandoSousaEsag@users.noreply.github.com>
2026-09-22 10:02:50 +0100

    Refactor aprovado_reprovado.py with comments
    Added comments to clarify the output format of the average.

--- 02_decisoes/aprovado_reprovado.py
+++ 02_decisoes/aprovado_reprovado.py
@@ -6,7 +6,7 @@ nota2 = float(input("Introduza a segunda nota: "))
 nota3 = float(input("Introduza a terceira nota: "))
 
 media = (nota1 + nota2 + nota3) / 3
-print(f"Média: {media:.1f}")
+print(f"Média: {media:.1f}") # imprime  média com uma casa decimal
 
 if media >= 10:
     print("Aluno aprovado")
